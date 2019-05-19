829ce817e7c4204994ac64c8f3238e0649cb78fa
Alex994 <zaraza9944@gmail.com>
2019-05-19 13:21:03 +0500

    Сделал модальные окна

---
+++ js/show_map.js
@@ -0,0 +1,23 @@
+var link_map = document.querySelector(".map_link");
+var popup_map = document.querySelector(".modal_map");
+var close_map = document.querySelector(".btn_map_close");
+
+link_map.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup_map.classList.add("modal_show");
+});
+
+close_map.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup_map.classList.remove("modal_show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+
+    if (popup_map.classList.contains("modal_show")) {
+      popup_map.classList.remove("modal_show");
+    }
+  }
+});
